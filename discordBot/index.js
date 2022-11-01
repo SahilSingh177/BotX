6e1172cba73e390e381fbcbb8bdd4fe9f276613a
Sahil <lcs2021033@iiitl.ac.in>
2022-11-02 01:29:01 +0530

    Added kick ban unban feature to the bot

--- discordBot/index.js
+++ discordBot/index.js
@@ -2,11 +2,15 @@ require("dotenv").config(); //initialize dotenv
 const Discord = require("discord.js"); //import discord.js
 const axios = require("axios");
 const client = new Discord.Client(); //create new client
+const { poll } = require("discord.js-poll"); //for poll using bot
+const ytdl = require('ytdl-core'); //For music
+const queue = new Map();
+
 
 const commands = {
-  "ping": ["Bot trial server"],
-  " aww": ["Bot trial server"],
-  "ugh": ["Bot trial server", "hope_'s server"],
+  ping: ["Bot trial server"],
+  "aww": ["Bot trial server"],
+  ugh: ["Bot trial server", "hope_'s server"],
   "!meme": ["hope_'s server", "Dark Domain"],
 };
 
@@ -26,13 +30,99 @@ function randomInt(min, max) {
 }
 
 client.on("message", async (msg) => {
+  let command = msg.content.split(" ")[0].slice(1);
+  let args = msg.content.replace("!" + command, "").trim();
+  switch (command) {
+    case "kick": {
+      let userID = args.includes("<@!")
+        ? args.replace("<@!", "").replace(">", "")
+        : args.includes("<@")
+        ? args.replace("<@", "").replace("<", "")
+        : "";
+      userID = userID.replace(">", "");
+      if (userID == "") {
+        msg.reply("Invalid user ID or mention.");
+        return;
+      }
+
+      msg.guild.members.fetch(userID).then((member) => {
+        member
+          .kick("Kicked by " + msg.author.tag)
+          .then((m) => {
+            msg.channel.send("👢 Kicked <@" + userID + ">.");
+          })
+          .catch(() => {
+            console.error;
+            msg.reply("Could not kick the specified member.");
+          });
+      });
+      break;
+    }
+    case "ban": {
+      let userID = args.includes("<@!")
+        ? args.replace("<@!", "").replace(">", "")
+        : args.includes("<@")
+        ? args.replace("<@", "").replace("<", "")
+        : "";
+      userID = userID.replace(">", "");
+      if (userID == "") {
+        msg.reply("Invalid user ID or mention.");
+        return;
+      }
+
+      msg.guild.members.fetch(userID).then((member) => {
+        member
+          .ban({ days: 7, reason: "They deserved it" })
+          .then(() => {
+            msg.channel.send("🔨 Banned <@" + userID + ">.");
+          })
+          .catch(() => {
+            console.error;
+            msg.reply("Could not ban the specified member.");
+          });
+      });
+      break;
+    }
+
+    case "unban": {
+      let userID = args.includes("<@!")
+        ? args.replace("<@!", "").replace(">", "")
+        : args.includes("<@")
+        ? args.replace("<@", "").replace("<", "")
+        : "";
+      userID = userID.replace(">", "");
+      if (userID == "") {
+        msg.reply("Invalid user ID or mention.");
+        return;
+      }
+
+      msg.guild
+        .fetchBans()
+        .then((bans) => {
+          let member = bans.get(userID);
+          if (member == null) {
+            msg.reply("Cannot find a ban for the given user.");
+            return;
+          }
+
+          msg.guild.members
+            .unban(userID)
+            .then(() => {
+              msg.channel.send("Unbanned <@" + userID + ">.");
+            })
+            .catch(console.error);
+        })
+        .catch(() => console.error);
+      break;
+    }
+  }
   switch (msg.content) {
     case "ping":
       if (commands["ping"].includes(msg.guild.name)) {
         msg.reply("Pong!");
         break;
       } else {
-        msg.reply("No such command exits bro!");
+        msg.reply("No such command exits bruh!");
         break;
       }
     case "ugh":
@@ -60,8 +150,7 @@ client.on("message", async (msg) => {
             msg.channel.send(`${title}\n${url}\n from ${subreddit}`);
           });
         break;
-      }
-      else {
+      } else {
         msg.reply("Naah Hard paas!");
         break;
       }
