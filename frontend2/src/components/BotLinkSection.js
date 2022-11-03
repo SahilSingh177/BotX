04165f672d152a87f063a853d206933cf94e4938
Sahil <lcs2021033@iiitl.ac.in>
2022-11-03 06:15:56 +0530

    added routers

--- frontend2/src/components/BotLinkSection.js
+++ frontend2/src/components/BotLinkSection.js
@@ -1,13 +1,10 @@
 import React from 'react';
-import {
-  Link
-} from "react-router-dom";
+
 import 'animate.css';
 
 export const BotLinkSec = () => {
   return (
     <section className="team" id="botsection">
-      {/* <Link to="#home">Wanna Build a Bot?</Link> */}
       </section>
   )
 }
--- frontend2/src/components/Container.js
+++ frontend2/src/components/Container.js
@@ -1,24 +1,19 @@
-import React, { Component } from 'react'
+import React from "react";
 
-import './container.css'
+import "./container.css";
 
-export class Container extends Component {
-  render() {
-    return (
-      <div className="main" id="botForm">
-        <div className="inputfield">
-          <p>Bot Name:</p>
-          <input type="text" />
-        </div>
-        <div className="inputfield">
-          <p>Bot Description:</p>
-          <input type="text" className='desc'/>
-        </div>
-        <button>Go!</button>
+export const Container = () =>{
+  return (
+    <div className="main" id="botForm">
+      <div className="inputfield">
+        <p>Bot Name:</p>
+        <input type="text" />
       </div>
-    )
-  }
+      <div className="inputfield">
+        <p>Bot Description:</p>
+        <input type="text" className="desc" />
+      </div>
+      <button>Go!</button>
+    </div>
+  );
 }
-
-export default Container;
-
