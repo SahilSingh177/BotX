fd01d9d7d4109e8315271192b6400bb4c0cb1cee
Sahil <lcs2021033@iiitl.ac.in>
2022-11-04 05:51:29 +0530

    Finalised discord bot

--- frontend/overengineered/src/navbarhamburger.js
+++
@@ -1,6 +0,0 @@
-const btn = document.getElementsByClassName('button')[0];
-const links = document.getElementsByClassName('links')[0];
-
-btn.addEventListener('click', ()=>{
-    links.classList.toggle('active');
-})
